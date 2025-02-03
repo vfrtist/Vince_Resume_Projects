83fc8d74a996e88c9a4e63ac13e2cbfa984a71a3
vfrtist <137813892+vfrtist@users.noreply.github.com>
2025-02-02 20:25:47 -0500

    Populated Projects. Started Landing Page

---
+++ Web/Artists Portfolio/gallery.js
@@ -0,0 +1,16 @@
+const main = document.querySelector('#gallery');
+
+function make(item) { return document.createElement(item.toString()); }
+
+
+for (let card of main.querySelectorAll('.card')) {
+    card.addEventListener('click', (e) => {
+        const modal = make('dialog');
+        const image = card.querySelector('img').cloneNode('true');
+        modal.classList.add('modal');
+        modal.append(image);
+        document.body.append(modal);
+        modal.showModal();
+        modal.addEventListener('click', () => { modal.remove(); })
+    })
+}
