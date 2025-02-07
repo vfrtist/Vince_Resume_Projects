70c2fc1e83d5df277b20382487bcd83b80edb880
vfrtist <vfrtist@aol.com>
2025-02-07 13:38:49 -0500

    Added scrolling functionality
    Made the presentation update on scroll.

--- app.js
+++ app.js
@@ -4,17 +4,46 @@
 // Since the scroll event with the menu will trigger the scroll event the class visuals accordingly
 
 const title = document.querySelector('#title')
-const pages = document.querySelectorAll('nav li a')
+const pageButtons = document.querySelectorAll('nav li a')
+const pageElements = document.querySelectorAll('.page')
 const main = document.querySelector('main')
 let currentPage = document.querySelector('.active')
 
-pages.forEach(nav => {
-    nav.addEventListener('click', () => {
-        if (currentPage) {
-            currentPage.classList.remove('active')
-        }
-        nav.classList.add('active')
-        title.innerHTML = nav.innerHTML
-        currentPage = nav
-    })
+let pages = [];
+
+class pageClass {
+    constructor (button, page) {
+        this.button = button
+        this.page = page
+        this.text = button.innerHTML
+    }
+
+    get top() {
+        return this.page.getBoundingClientRect().y
+    }
+
+    makeActive() {
+        title.innerHTML = this.text
+        this.button.classList.add('active')
+    }
+
+    makeInactive() {
+        this.button.classList.remove('active')
+    }
+}
+
+for (let index = 0; index < pageButtons.length; index++) {
+    pages.push(new pageClass(pageButtons[index], pageElements[index]))
+}
+
+main.addEventListener('scroll', (e) => {
+    const height = e.target.clientHeight
+    console.log(height)
+    console.log('scroll\n')
+    for(let page of pages) {
+        if (0 < page.top  && page.top < height) {
+            page.makeActive()
+            break
+        } else { page.makeInactive() }
+    }
 })
