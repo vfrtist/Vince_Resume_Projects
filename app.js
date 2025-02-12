f5c495e45759f520483bd1fe97ef015c90eadf43
vfrtist <vfrtist@aol.com>
2025-02-12 14:43:50 -0500

    Created dynamic Popup class

--- app.js
+++ app.js
@@ -48,31 +48,102 @@ main.addEventListener('scroll', (e) => {
 
 //=============== WebOpener ====================
 const webLinks = document.querySelectorAll('.web')
+const resizing = false
 
 function isValidWidth() {
     return window.innerWidth > 850
+}
+
+class Popup {
+    constructor (target) {
+        this.isResizing = false
+
+        this.container = make('dialog')
+        this.title = target.closest('.line').querySelector('h4').innerHTML
+        this.type = target.closest('a').getAttribute('value')
+        this.href = target.closest('a').href
+        this.close = make('span')
+        this.dragBar;
+
+        this.setup()
+    }
+
+    setup() {
+        const frame = make('iframe')
+        const newTitle = make('h4')
+
+        this.close.innerHTML = 'X'
+        this.container.classList.add(this.type)
+        frame.src = this.href
+        newTitle.innerHTML = this.title
+
+        this.container.append(newTitle)
+        this.container.append(frame)
+        this.container.append(this.close)
+        document.body.append(this.container)
+
+        this.close.addEventListener('click', () => { 
+            this.container.remove()
+            delete this
+        })
+
+        if (this.type == 'dynamic') {this.makeDynamic()}
+
+        this.container.showModal()
+
+    }
+
+    makeDynamic() {
+        this.dragBar = make('div')
+        this.dragBar.classList.add('draggable')
+        this.container.append(this.dragBar)
+
+        this.dragBar.addEventListener('mousedown', (e) => {
+            e.preventDefault()
+            this.startSize()
+        })
+        window.addEventListener('mouseup', () => {this.stopSize()})
+        window.addEventListener('mousemove', (e) => {
+            if (this.isResizing) {
+                e.preventDefault()
+                this.resize(e.clientX)
+            }
+        })
+        
+        this.dragBar.addEventListener('touchstart', (e) => {
+            e.preventDefault()
+            this.startSize()
+        })        
+        window.addEventListener('touchend', () => {this.stopSize()})
+        window.addEventListener('touchmove', (e) => {
+            if (this.isResizing) {
+                e.preventDefault()
+                this.resize(e.clientX)
+            }
+        })
+
+    }
+
+    startSize() {
+        this.isResizing = true
+    }
+
+    stopSize() {
+        this.isResizing = false
+    }
+
+    resize(val) {    this.container.style.width = `${window.innerWidth - 2*(window.innerWidth - val + 20)}px`    }
 }
 
 webLinks.forEach(button => {
     button.addEventListener('click', (e)=> {
         const button  = e.target.closest('a')
         if (!isValidWidth() && button.getAttribute('value') == 'desktop') {
+            // do nothing because it should act normal
         } else {
         e.preventDefault()
-        const title = e.target.closest('.line').querySelector('h4').innerHTML
-        const modal = make('dialog')
-        const frame = make('iframe')
-        const newTitle = make('h4')
-
-        modal.classList.add(button.getAttribute('value'))
-        frame.src = button.href
-        newTitle.innerHTML = title
-        
-        modal.append(newTitle)
-        modal.append(frame)
-        document.body.append(modal)
-        modal.showModal()
-        modal.addEventListener('click', () => { modal.remove()})
+        new Popup(e.target)
         }
+
     })
 });
